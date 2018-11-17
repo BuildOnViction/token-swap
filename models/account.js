dbf58e33bfec2dc163393fc94f3e2fcb37372ac0
Minh Khai Do <khaihkd@gmail.com>
2018-11-17 14:33:16 +0700

    update mode

--- models/account.js
+++ models/account.js
@@ -7,7 +7,9 @@ const Account = new Schema({
     hash: { type: String, unique: true },
     balance: String,
     balanceNumber: Number,
-    accountType: { type: String, index: true }
+    accountType: { type: String, index: true },
+    isSend: { type: Boolean, index: true, default: false },
+    hasBalance: { type: Boolean, index: true, default: false },
 }, { timestamps: false })
 
 module.exports = mongoose.model('Account', Account)
